1e31595ea952fbe9d6ac6b0f9e6a8b5d2df25a5f
Surendra Tiwari <surendratiwari3@gmail.com>
2018-12-14 16:41:19 +0530

    origination uuid added

--- requests/repository/request.go
+++ requests/repository/request.go
@@ -30,21 +30,20 @@ func (a *Controller) call(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, response)
 	}
 
-	callUUID, err :=  coreUtils.GenUUID()
-	if err != nil{
+	callUUID, err := coreUtils.GenUUID()
+	if err != nil {
 		panic("not able to generate the connection UUID to connect with FreeSWITCH")
 	}
 	response["callUUID"] = callUUID
-	
-	authId := c.Param("auth_id")
+
+	//authId := c.Param("auth_id")
 	fromNumber := callDetails["fromNumber"].(string)
 	toNumber := callDetails["toNumber"].(string)
 	didNumber := callDetails["didNumber"].(string)
 
-	response["message"] = "Successfully Authenticated " + authId
-	eslJobid := a.ESLClient.BgApi(fmt.Sprintf("originate %s %s",
+	response["message"] = "Call is Created"
+	a.ESLClient.BgApi(fmt.Sprintf("originate %s %s",
 		"{origination_uuid="+callUUID+",origination_caller_id_number="+didNumber+",absolute_codec_string=PCMU,PCMA}sofia/internal/"+toNumber+"@10.17.112.21",
 		"&bridge({origination_caller_id_number="+didNumber+",absolute_codec_string=PCMU,PCMA}sofia/external/"+fromNumber+"@10.17.112.21)"))
-	response["jobId"] = eslJobid
 	return c.JSON(http.StatusOK, response)
 }
